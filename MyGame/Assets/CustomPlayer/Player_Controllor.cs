dcbb9492422e8878441d274cc84f9a8249298782
admin <>
2023-03-07 13:45:24 +0900

    Unity 23.03.07

--- MyGame/Assets/CustomPlayer/Player_Controllor.cs
+++ MyGame/Assets/CustomPlayer/Player_Controllor.cs
@@ -4,21 +4,90 @@ using UnityEngine;
 
 public class Player_Controllor : MonoBehaviour
 {
-    private float Speed;
+    private float _speed;
+    private Vector3 _moveMent;
+    private bool _onAttack;
+    private bool _onHit;
+    private bool _onJump;
 
-    void Start()
+    public Animator _animator;
+    void Start() //Init()
     {
-        Speed = 5.0f;
+        _speed = 5.0f;
+        SetState();
     }
 
     void Update()
     {
-        float Har = Input.GetAxis("Horizontal");
-        float Ver = Input.GetAxis("Vertical");
+        // ** Player의 Animator를 받아온다
+        _animator = this.GetComponent<Animator>();
+        Run();
 
-        transform.position += new Vector3(
-             Har * Time.deltaTime * Speed
-            ,Ver * Time.deltaTime * Speed
-            ,0.0f);
+        if (Input.GetKey(KeyCode.LeftControl))
+            OnAttack();
+        if (Input.GetKey(KeyCode.LeftShift))
+            OnHit();
+        if (Input.GetKey(KeyCode.Space))
+            OnJump();
+    }
+    private void SetState()
+    {
+        _onAttack = false;
+        _onHit = false;
+        _onJump = false;       
+    }
+
+    private void Run()
+    {
+        // ** Input.GetAxis =  -1, 1의 값을 반환
+        // ** 실수 연산 IEEE754
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        _moveMent = new Vector3(
+             hor * _speed,
+             ver * _speed,
+             0.0f);
+
+        _animator.SetFloat("Speed", Mathf.Abs(hor));
+        this.transform.position += _moveMent * Time.deltaTime;
+    }
+
+    private void OnHit()
+    {
+        if (_onHit) return;
+
+        _onHit = true;
+        _animator.SetTrigger("Hit");
+    }
+
+    private void OnAttack()
+    {
+        if (_onAttack) return;
+
+        _onAttack = true;
+        _animator.SetTrigger("Attack");
+    }
+
+    private void OnJump()
+    {
+        if (_onJump) return;
+
+        _onJump = true;
+        _animator.SetTrigger("Jump");
+    }
+    
+    private void SetJump()
+    {
+        SetState();
+    }
+    private void SetAttack()
+    {
+        _onAttack = false;
+    }
+
+    private void SetHit()
+    {
+        SetState();
     }
 }
